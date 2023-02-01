71a476e6d9b43e53b59907fb6f7f606ed424c799
Silvio Limeira <sicemal@gmail.com>
2023-02-01 16:15:58 -0400

    Products from Resultset

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,14 +1,18 @@
 package app;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import db.DB;
+import db.DbException;
 import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) throws SQLException {
 		
-		/*
 		Connection conn = DB.getConnection();
 	
 		Statement st = conn.createStatement();
@@ -16,14 +20,28 @@ public class Program {
 		ResultSet rs = st.executeQuery("select * from tb_product");
 			
 		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+			
+			Product p = instantiateProduct(rs);
+			
+			//System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+			System.out.println(p);
 		}
-		*/
 		
+		
+	}
+
+	private static Product instantiateProduct(ResultSet rs) {
 		Product p = new Product();
-		p.setName("Pizza");
-		
-		System.out.println("Nome do produto = " + p.getName());
-		
+		try {
+			p.setId(rs.getLong("id"));
+			p.setDescription(rs.getString("description"));
+			p.setName(rs.getString("name"));
+			p.setImageUri(rs.getString("image_uri"));
+			p.setPrice(rs.getDouble("price"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e.getMessage());
+		}
+		return p;
 	}
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Product {
 
 	private Long id;
@@ -7,13 +9,82 @@ public class Product {
 	private Double price;
 	private String description;
 	private String imageUri;
-	
+
+	public Product() {
+
+	}
+
+	public Product(Long id, String name, Double price, String description, String imageUri) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, imageUri, name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(imageUri, other.imageUri) && Objects.equals(name, other.name)
+				&& Objects.equals(price, other.price);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", description=" + description
+				+ ", imageUri=" + imageUri + "]";
 	}
 	
 	
